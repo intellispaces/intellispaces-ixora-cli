e1c057446b28d1cdc9a6a9591d5d07728c3f6c0c
lavor <lavor.andrey@gmail.com>
2024-08-22 14:21:42 +0600

    Join modules ixora-cli-ontology and ixora-cli

---
+++ ixora-cli-ontology/src/main/java/intellispaces/ixora/cli/Console.java
@@ -0,0 +1,25 @@
+package intellispaces.ixora.cli;
+
+import intellispaces.ixora.core.Point;
+import tech.intellispaces.core.annotation.Domain;
+import tech.intellispaces.core.annotation.Transition;
+import tech.intellispaces.core.traverse.TraverseTypes;
+
+/**
+ * Console is a simple text in/out system port.
+ */
+@Domain("ac5ad94b-0d0b-4bc1-b0af-a32c65a28793")
+public interface Console extends Point {
+
+  @Transition(value = "e09d266d-9568-44fd-8ed2-cf27da326a37", name = "ConsolePrintStringTransition", allowedTraverseTypes = TraverseTypes.Moving)
+  Void print(String string);
+
+  @Transition(value = "5c0ac95a-a8f9-4a77-a6f7-7897659e80b1", name = "ConsolePrintIntegerTransition", allowedTraverseTypes = TraverseTypes.Moving)
+  Void print(int number);
+
+  @Transition(value = "0df1747b-b0dc-4a34-9584-54d06af4ffe2", name = "ConsolePrintlnStringTransition", allowedTraverseTypes = TraverseTypes.Moving)
+  Void println(String string);
+
+  @Transition(value = "1e2ffbd9-dd81-4fa5-8658-93052623c1b9", name = "ConsolePrintlnIntegerTransition", allowedTraverseTypes = TraverseTypes.Moving)
+  Void println(int number);
+}
