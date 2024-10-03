8cf44f457c7098945d18b8189d9326b185432f0c
lavor <lavor.andrey@gmail.com>
2024-10-03 10:53:59 +0600

    Update AbstractPrintStreamBasedConsole

--- intellispaces-ixora-cli/src/main/java/intellispaces/ixora/cli/AbstractPrintStreamBasedConsole.java
+++ intellispaces-ixora-cli/src/main/java/intellispaces/ixora/cli/AbstractPrintStreamBasedConsole.java
@@ -4,6 +4,7 @@ import intellispaces.framework.core.annotation.Mover;
 import intellispaces.framework.core.annotation.ObjectHandle;
 
 import java.io.PrintStream;
+import java.util.Objects;
 
 @ObjectHandle(value = ConsoleDomain.class, name = "PrintStreamBasedConsole")
 public abstract class AbstractPrintStreamBasedConsole implements MovableConsole {
@@ -13,9 +14,14 @@ public abstract class AbstractPrintStreamBasedConsole implements MovableConsole 
     this.ps = ps;
   }
 
+  public PrintStream getPrintStream() {
+    return ps;
+  }
+
   @Mover
   @Override
   public Console print(String string) {
+    Objects.requireNonNull(string);
     ps.print(string);
     return this;
   }
@@ -30,6 +36,7 @@ public abstract class AbstractPrintStreamBasedConsole implements MovableConsole 
   @Mover
   @Override
   public Console println(String string) {
+    Objects.requireNonNull(string);
     ps.println(string);
     return this;
   }
