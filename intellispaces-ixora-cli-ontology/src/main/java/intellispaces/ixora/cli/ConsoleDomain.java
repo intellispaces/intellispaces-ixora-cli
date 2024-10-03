9d7f13a495d49a446ed9432cefa61ec1e058ea49
lavor <lavor.andrey@gmail.com>
2024-10-03 09:11:35 +0600

    Rename transition to channel

--- intellispaces-ixora-cli-ontology/src/main/java/intellispaces/ixora/cli/ConsoleDomain.java
+++ intellispaces-ixora-cli-ontology/src/main/java/intellispaces/ixora/cli/ConsoleDomain.java
@@ -13,28 +13,28 @@ public interface ConsoleDomain extends PointDomain {
 
   @Channel(
       value = "e09d266d-9568-44fd-8ed2-cf27da326a37",
-      name = "ConsolePrintStringTransition",
+      name = "ConsolePrintStringChannel",
       allowedTraverse = TraverseTypes.Moving
   )
   ConsoleDomain print(String string);
 
   @Channel(
       value = "5c0ac95a-a8f9-4a77-a6f7-7897659e80b1",
-      name = "ConsolePrintIntegerTransition",
+      name = "ConsolePrintIntegerChannel",
       allowedTraverse = TraverseTypes.Moving
   )
   ConsoleDomain print(int number);
 
   @Channel(
       value = "0df1747b-b0dc-4a34-9584-54d06af4ffe2",
-      name = "ConsolePrintlnStringTransition",
+      name = "ConsolePrintlnStringChannel",
       allowedTraverse = TraverseTypes.Moving
   )
   ConsoleDomain println(String string);
 
   @Channel(
       value = "1e2ffbd9-dd81-4fa5-8658-93052623c1b9",
-      name = "ConsolePrintlnIntegerTransition",
+      name = "ConsolePrintlnIntegerChannel",
       allowedTraverse = TraverseTypes.Moving
   )
   ConsoleDomain println(int number);
